ca3dc71235deb4746e7230f48194b0be33ab8691
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2022-01-12 20:02:35 +0100

    parcel, prettier, eslint, react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to Coders Camp 2021</h1>
+      <h2>SPA Project</h2>
+    </>
+  );
+};
+
+export default App;
